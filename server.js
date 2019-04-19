04c7eb660d6206a4d90ee8d08eb094e0e363b66b
Eddie Chu <contact@eddiechu.me>
2019-04-18 23:42:07 -0700

    initial server and db setup

---
+++ server.js
@@ -0,0 +1,67 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const app = express();
+
+// CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.send(204);
+    }
+    next();
+});
+
+app.use('*', (req, res) => {
+    return res.status(404).json({ message: 'Not Found' });
+});
+
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
+
+function runServer() {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DATABASE_URL, {useNewUrlParser: true}, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
